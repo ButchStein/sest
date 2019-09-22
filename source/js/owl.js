f4899168cd0d92c6ef891b7588633011c1373d9f
ButchStein <butchbs1982@gmail.com>
2019-09-22 18:12:30 +0300

    initial

---
+++ source/js/owl.js
@@ -0,0 +1,19 @@
+// $(document).ready(function () {
+//   $(".owl-carousel").owlCarousel();
+// });
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:1,
+    loop:true,
+    margin:0,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true,
+    dots:true
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
