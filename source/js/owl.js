0219d9f563fc553415b503a1a0dd89da672b2c92
ButchStein <butchbs1982@gmail.com>
2019-09-23 09:12:51 +0300

    a

--- source/js/owl.js
+++ source/js/owl.js
@@ -1,19 +1,16 @@
-// $(document).ready(function () {
-//   $(".owl-carousel").owlCarousel();
-// });
 var owl = $('.owl-carousel');
 owl.owlCarousel({
-    items:1,
-    loop:true,
-    margin:0,
-    autoplay:true,
-    autoplayTimeout:5000,
-    autoplayHoverPause:true,
-    dots:true
+  items: 1,
+  loop: true,
+  margin: 0,
+  autoplay: false,
+  autoplayTimeout: 5000,
+  autoplayHoverPause: true,
+  dots: true
 });
-$('.play').on('click',function(){
-    owl.trigger('play.owl.autoplay',[1000])
-})
-$('.stop').on('click',function(){
-    owl.trigger('stop.owl.autoplay')
-})
+$('.play').on('click', function () {
+  owl.trigger('play.owl.autoplay', [1000])
+});
+$('.stop').on('click', function () {
+  owl.trigger('stop.owl.autoplay')
+});
