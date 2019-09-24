0fa1709753c9f12b483aeea37579eedc8bb2a536
ButchStein <butchbs1982@gmail.com>
2019-09-24 03:50:52 +0300

    e

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,10 @@
-var modal = document.querySelector("#modal"),
-    modalOverlay = document.querySelector("#modal-overlay"),
-    closeButton = document.querySelector("#close-button"),
-    openButton = document.querySelector("#open-button");
- 
-closeButton.addEventListner("click", function(){
-   modal.classList.toggle("closed");
-   modalOverlay.classList.toggle("closed");
+var elements = $('.modal-overlay, .modal');
+
+$('#open-button').click(function(){
+    elements.addClass('active');
+
 });
- 
-openButton.addEventListner("click", function(){
-   modal.classList.toggle("closed");
-   modalOverlay.classList.toggle("closed");
+
+$('#close-button').click(function(){
+    elements.removeClass('active');
 });
